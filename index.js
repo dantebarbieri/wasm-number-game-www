fb44b8b9dd22e5af8dd75466c2542f4a60c9ee0d
dantebarbieri <dantevbarbieri@gmail.com>
2024-08-27 23:04:51 -0600

    add visuals and game

--- index.js
+++ index.js
@@ -1,35 +1,85 @@
 import * as wasm from "wasm-number-game";
 
 const startButton = document.getElementById("start-button");
+const nextOutput = document.getElementById("next");
 const gameSummary = document.getElementById("game-summary");
 const progressLabel = document.getElementById("progress-label");
 const progressBar = document.getElementById("progress-bar");
+const slots = document.getElementsByClassName("slot");
 
-// Function to start the game
 function startGame() {
-  // Hide the start button
   startButton.style.display = "none";
 
-  // Show the game summary section
-  gameSummary.style.display = "block";
+  gameSummary.style.display = "flex";
 
-  progressLabel.innerText = "0/20";
-  progressBar.innerText = progressLabel.innerText;
-  progressBar.value = 0;
+  getNextRandom();
 
-  // Example: Simulate game over after some time
-  setTimeout(endGame, 5000); // Game lasts 5 seconds for example
+  for (const slot of slots) {
+    slot.disabled = false;
+    slot.addEventListener("click", step);
+  }
 }
 
-// Function to end the game
 function endGame() {
-  // Show the start button again
   startButton.style.display = "block";
+}
 
-  // The game summary remains visible
+function step(e) {
+  e.target.disabled = true;
+  e.target.innerText = nextOutput.innerText;
+  getNextRandom();
+  let s = slots.length;
+  for (; s > 0; --s) {
+    if (
+      slots[s - 1].innerText &&
+      +slots[s - 1].innerText < +nextOutput.innerText
+    ) {
+      break;
+    }
+  }
+
+  let t = 0;
+  if (!(slots[0].innerText && slots[0].innerText > nextOutput.innerText)) {
+    for (; t + 1 < slots.length; ++t) {
+      if (
+        slots[t + 1].innerText &&
+        +slots[t + 1].innerText > +nextOutput.innerText
+      ) {
+        break;
+      }
+    }
+  }
+
+  let any = false;
+  for (let i = 0; i < slots.length; ++i) {
+    slots[i].disabled = !(s <= i && i <= t);
+    any ||= !slots[i].disabled;
+  }
+
+  if (!any) {
+    endGame();
+  }
+}
+
+function getNextRandom() {
+  nextOutput.innerText = wasm.next(1, 1000);
+
+  progressLabel.innerText = `${getNumberFilledSlots()}/${slots.length}`;
+  progressBar.innerText = progressLabel.innerText;
+  progressBar.value = getNumberFilledSlots();
+}
+
+function getNumberFilledSlots() {
+  let acc = 0;
+
+  for (const slot of slots) {
+    if (slot.innerText) {
+      acc++;
+    }
+  }
+
+  return acc;
 }
 
 // Attach click event to the start button
 startButton.addEventListener("click", startGame);
-
-wasm.greet();
